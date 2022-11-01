b68005a5aa034153db7d194c7e9d462a98c82a14
demba65 <117170769+demba65@users.noreply.github.com>
2022-11-01 18:40:15 +0200

    Add files via upload

---
+++ First Steps In Coding - Exercise/08. Basketball Equipment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearTax = int.Parse(Console.ReadLine());
+
+            double basketballShoes = yearTax - (0.4 * yearTax);
+            double basketballSuit = basketballShoes - (0.2 * basketballShoes);
+            double basketballBall = basketballSuit * 0.25;
+            double basketballAccessories = basketballBall * 0.2;
+
+            double sum = basketballShoes + basketballSuit + basketballBall + basketballAccessories + yearTax;
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ First Steps In Coding - Exercise/09. Fish Tank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double lengthCm = double.Parse(Console.ReadLine());
+            double widthCm = double.Parse(Console.ReadLine());
+            double heightCm = double.Parse(Console.ReadLine());
+            double Procent = double.Parse(Console.ReadLine());
+            
+            double vCm = lengthCm * widthCm * heightCm;
+            double vLit = vCm / 1000;
+            double a = Procent / 100;
+            double liters = vLit * (1 - a);
+
+            Console.WriteLine(liters);
+        }
+    }
+}
