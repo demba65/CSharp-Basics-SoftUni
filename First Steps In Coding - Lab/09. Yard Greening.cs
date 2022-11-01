302eeac97d3feb0cf365e63f3fdaf3beca222947
demba65 <117170769+demba65@users.noreply.github.com>
2022-11-01 18:47:04 +0200

    Update 09. Yard Greening.cs

--- First Steps In Coding - Lab/09. Yard Greening.cs
+++ First Steps In Coding - Lab/09. Yard Greening.cs
@@ -6,8 +6,8 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            double kvMetri = double.Parse(Console.ReadLine());
-            double sum = kvMetri * 7.61;
+            double squareMetre = double.Parse(Console.ReadLine());
+            double sum = squareMetre * 7.61;
             double discount = 0.18 * sum;
             double sumDiscount = sum - discount;
             Console.WriteLine($"The final price is: {sumDiscount} lv.");
