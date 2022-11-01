59df141c4d75a138e543dea6e21eeb3f2a429e6c
demba65 <117170769+demba65@users.noreply.github.com>
2022-11-01 16:48:51 +0200

    Add files via upload

---
+++ First Steps In Coding - Exercise/07. Food Delivery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenu = int.Parse(Console.ReadLine());
+            int fishMenu = int.Parse(Console.ReadLine());
+            int vegetarianMenu = int.Parse(Console.ReadLine());
+
+            double chickenMenuPrice = chickenMenu * 10.35;
+            double fishMenuPrice = fishMenu * 12.40;
+            double vegetarianMenuPrice = vegetarianMenu * 8.15;
+            double desertPrice = (chickenMenuPrice + fishMenuPrice + vegetarianMenuPrice) * 0.2;
+            double delivery = 2.50;
+
+            double deliveryPrice = chickenMenuPrice + fishMenuPrice + vegetarianMenuPrice + desertPrice + delivery;
+            Console.WriteLine(deliveryPrice);
+        }
+    }
+}
