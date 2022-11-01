dbe9964c6f72f80e719d3dadc1c2bedba5926f86
demba65 <117170769+demba65@users.noreply.github.com>
2022-11-01 18:43:48 +0200

    Update 03. Rectangle Area.cs

--- First Steps In Coding - Lab/03. Rectangle Area.cs
+++ First Steps In Coding - Lab/03. Rectangle Area.cs
@@ -6,8 +6,8 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            double kvMetri = double.Parse(Console.ReadLine());
-            double sum = kvMetri * 7.61;
+            double squareMetre = double.Parse(Console.ReadLine());
+            double sum = squareMetre * 7.61;
             double otstupka = (sum) / (18 / 100);
             Console.WriteLine($"The final price is: {sum} lv.");
             Console.WriteLine($"The discount is: {otstupka} lv.");
