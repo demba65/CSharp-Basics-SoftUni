3825832bb5f15576d06f0de176864f10b63d9f86
demba65 <117170769+demba65@users.noreply.github.com>
2022-11-04 16:56:23 +0200

    Update 05. Supplies for School.cs

--- First Steps In Coding - Exercise/05. Supplies for School.cs
+++ First Steps In Coding - Exercise/05. Supplies for School.cs
@@ -6,16 +6,16 @@ namespace _05._Supplies_for_School
     {
         static void Main(string[] args)
         {
-            int brPaketiHimikali = int.Parse(Console.ReadLine());
-            int brPaketiMarkeri = int.Parse(Console.ReadLine());
-            int litriPreparatZaPochistvaneNaDuska = int.Parse(Console.ReadLine());
-            int procentNamalenie = int.Parse(Console.ReadLine());
-            double himikaliCena = 5.80;
-            double markeriCena = 7.20;
-            double liturPreparatCena = 1.20;
-            double obshtaSuma = (brPaketiHimikali * himikaliCena) + (brPaketiMarkeri * markeriCena) + (litriPreparatZaPochistvaneNaDuska * liturPreparatCena);
-            double nujniPari = obshtaSuma - (obshtaSuma * procentNamalenie / 100);
-            Console.WriteLine(nujniPari);
+            int paketsPens = int.Parse(Console.ReadLine());
+            int paketsMarkers = int.Parse(Console.ReadLine());
+            int litrePreparation = int.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+            double pensPrice = 5.80;
+            double markersPrice = 7.20;
+            double litrePreparationPrice = 1.20;
+            double sum = (paketsPens * pensPrice) + (paketsMarkers * markersPrice) + (litrePreparation * litrePreparationPrice);
+            double money = sum - (sum * discount / 100);
+            Console.WriteLine(money);
         }
     }
 }
